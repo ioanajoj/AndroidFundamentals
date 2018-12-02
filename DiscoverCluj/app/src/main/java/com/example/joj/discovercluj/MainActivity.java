8cab1daad10776e36d3f2594b26b4d6cb50f9175
Ioana Gheorghiu <ioanna.gheorghiu@gmail.com>
2018-12-03 01:09:26 +0200

    Revert "Settings window"

--- DiscoverCluj/app/src/main/java/com/example/joj/discovercluj/MainActivity.java
+++ DiscoverCluj/app/src/main/java/com/example/joj/discovercluj/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.joj.discovercluj;
 
-import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
+        // programmatically set font to title text
+        TextView textview = (TextView) findViewById(R.id.title_textView);
+        Typeface font = Typeface.createFromAsset(getApplicationContext().getAssets(), "font/yanonekaffeesatz_regular.ttf");
+        textview.setTypeface(font);
 
         final ImageButton firstButton = (ImageButton) findViewById(R.id.image_button_1);
         firstButton.setOnClickListener(new View.OnClickListener() {
@@ -59,9 +65,6 @@ public class MainActivity extends AppCompatActivity {
                 String msg = "Access settings";
                 Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
                 toast.show();
-
-                Intent intent = new Intent(MainActivity.this, OpenSettings.class);
-                startActivity(intent);
             }
         });
 
